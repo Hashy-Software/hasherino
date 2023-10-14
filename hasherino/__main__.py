04a88292aa6b8de0eb1f954e5b5f52ffb7d750b7
douglascdev <douglasc.dev@gmail.com>
2023-10-14 15:46:40 -0300

    Move StatusColumn to a separate module

--- hasherino/__main__.py
+++ hasherino/__main__.py
@@ -10,6 +10,7 @@ from typing import Any, Awaitable, Coroutine
 import flet as ft
 
 from hasherino import helix, user_auth
+from hasherino.components import StatusColumn
 from hasherino.hasherino_dataclasses import Emote, EmoteSource, HasherinoUser, Message
 from hasherino.helix import NormalUserColor
 from hasherino.storage import AsyncKeyValueStorage, MemoryOnlyStorage, PersistentStorage
@@ -793,42 +794,6 @@ class Hasherino:
                     await self.persistent_storage.get("token"),
                 ),
             )
-
-
-class StatusColumn(ft.Column):
-    def __init__(
-        self,
-        memory_storage: AsyncKeyValueStorage,
-        persistent_storage: AsyncKeyValueStorage,
-    ):
-        self.reconnecting_status = ft.Row(
-            vertical_alignment=ft.CrossAxisAlignment.CENTER,
-            controls=[
-                ft.ProgressRing(width=16, height=16, stroke_width=2),
-                ft.Text("Reconnecting..."),
-            ],
-        )
-
-        self.memory_storage = memory_storage
-        self.persistent_storage = persistent_storage
-
-        super().__init__()
-
-    async def set_reconnecting_status(self, reconnecting: bool):
-        await self.memory_storage.set("reconnecting", reconnecting)
-
-        if reconnecting:
-            self.controls.append(self.reconnecting_status)
-        else:
-            if self.reconnecting_status in self.controls:
-                self.controls.remove(self.reconnecting_status)
-
-            channel = await self.persistent_storage.get("channel")
-            if channel:
-                websocket = await self.memory_storage.get("websocket")
-                await websocket.join_channel(channel)
-
-        await self.page.update_async()
 
 
 async def main(page: ft.Page):
